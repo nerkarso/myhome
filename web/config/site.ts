dd448013f6372b738ad4fd1d5439a1ec290b8d57
Ner Karso <nerpkarso@gmail.com>
2023-04-23 10:30:27 -0300

    replace project with Vite

--- web/config/site.ts
+++
@@ -1,9 +0,0 @@
-interface SiteConfig {
-  title: string;
-  description: string;
-}
-
-export const siteConfig: SiteConfig = {
-  title: 'MyHome',
-  description: 'The place to be for all listings of properties and homes for sale.',
-};
